b4b7e23930c179907a3151888ff81fdf3a36c1f9
bloriot97 <bloriot97@gmail.com>
2019-12-08 13:21:43 -0500

    First commit

---
+++ docs/precache-manifest.fe8e4c84ff25e06e1f3fd0f7949dc668.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ce516a3dc6eefe7f02a752085f5da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673e7c443c335ac5cb33",
+    "url": "/static/js/2.2ca319b2.chunk.js"
+  },
+  {
+    "revision": "0bfcbcac59c1c35525fc3a380d44c2a0",
+    "url": "/static/js/2.2ca319b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d585aca7b469db18033a",
+    "url": "/static/js/main.7fedeae0.chunk.js"
+  },
+  {
+    "revision": "8d9780ddfcfcaa577e41",
+    "url": "/static/js/runtime-main.17d3968d.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,76 @@
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import name_info from './data_3D_sample.json';
+
+const scaleX = 5,
+    scaleY = 5,
+    scaleZ = 5;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        scene: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.createScene();
+  }   
+
+  perc2color(perc,min,max) {
+    var base = (max - min);
+
+    if (base == 0) { perc = 100; }
+    else {
+        perc = (perc - min) / base * 100; 
+    }
+    var r, g, b = 0;
+    if (perc < 50) {
+        r = 255;
+        g = Math.round(5.1 * perc);
+    }
+    else {
+        g = 255;
+        r = Math.round(510 - 5.10 * perc);
+    }
+    var h = r * 0x10000 + g * 0x100 + b * 0x1;
+    return '#' + ('000000' + h.toString(16)).slice(-6);
+  }
+
+  createScene() {
+    let names = [];
+    for (let name of name_info) {
+        names.push(<Entity 
+            primitive="a-box" 
+            position={name.x/scaleX + " " + name.y/scaleY + " " + name.z/scaleZ} 
+            height="0.1" 
+            width="0.1" 
+            depth="0.1" 
+            color={this.perc2color(parseInt(name.idxmax), 1900, 2019)}
+        />)
+    }
+    this.setState({
+        scene: names
+    })
+  }
+
+  render() {
+    return (
+        <Scene>
+            <a-sky color="#000000"></a-sky>
+            <Entity primitive="a-light" type="ambient" color="#445451"/>
+            <Entity primitive="a-light" type="directional" position="0 0 0" rotation="-90 0 0" target="#directionaltarget">
+                <Entity primitive="a-entity" id="directionaltarget" position="-1 -0.5 -0.75"></Entity>
+            </Entity>
+            {this.state.scene}
+        </Scene>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#sceneContainer'));
