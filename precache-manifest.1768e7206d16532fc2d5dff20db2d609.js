3d3debadc20a61ada56cfacfd99030ea25c58a48
bloriot97 <bloriot97@gmail.com>
2019-12-08 13:27:46 -0500

    Updates

---
+++ precache-manifest.1768e7206d16532fc2d5dff20db2d609.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f422790adbd7895778ae6285a1f8b1",
+    "url": "/namesVR/index.html"
+  },
+  {
+    "revision": "673e7c443c335ac5cb33",
+    "url": "/namesVR/static/js/2.2ca319b2.chunk.js"
+  },
+  {
+    "revision": "0bfcbcac59c1c35525fc3a380d44c2a0",
+    "url": "/namesVR/static/js/2.2ca319b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d585aca7b469db18033a",
+    "url": "/namesVR/static/js/main.7fedeae0.chunk.js"
+  },
+  {
+    "revision": "24787e3440a5f407c591",
+    "url": "/namesVR/static/js/runtime-main.fa292167.js"
+  }
+]);
